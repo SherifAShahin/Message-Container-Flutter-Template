cd39a61afdb2c3f6e32be92ea62d16bf5e7bc6fa
Sherif A Shahin <125300187+SherifAShahin@users.noreply.github.com>
2023-02-19 18:59:06 +0200

    Update message_container_template.dart

--- message_container_template.dart
+++ message_container_template.dart
@@ -1,4 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+Widget defaultButton({
+  double width = 280.0,
+  double height = 64.0,
+  String hexColor = '#045255',
+  double borderRadius = 15.0,
+  required Function()? onPressed,
+  required String text,
+  Color textColor = Colors.white,
+  double fontSize = 25.0,
+}) => Container(
+  width: width,
+  height: height,
+  decoration: BoxDecoration(
+    color: HexColor(
+      hexColor,
+    ),
+    borderRadius: BorderRadius.circular(
+      borderRadius,
+    ),
+  ),
+  child: MaterialButton(
+    onPressed: onPressed,
+    child: Text(
+      text,
+      style: TextStyle(
+        color: textColor,
+        fontSize: fontSize,
+      ),
+    ),
+  ),
+);
 
 Widget defaultNoteMessage({
   double margin = 25.0,
