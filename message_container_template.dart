96784f03e6e8478ff71428caa6d1d995fe4498b6
Sherif A Shahin <125300187+SherifAShahin@users.noreply.github.com>
2023-02-19 17:43:13 +0200

    Create message_container_template.dart

---
+++ message_container_template.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+Widget defaultNoteMessage({
+  double margin = 25.0,
+  double padding = 20.0,
+  double opacity = 0.5,
+  double borderRadius = 15.0,
+  double titleBoldTextSize = 30.0,
+  double titleRegularTextSize = 20.0,
+  double bodyTextSize = 20.0,
+  double endTextSize = 20.0,
+  bool isTransparent = true,
+  Color backgroundColor = Colors.white,
+  Color? titleBoldTextColor = Colors.black,
+  Color? titleRegularTextColor = Colors.black,
+  Color? bodyTextColor = Colors.black,
+  Color? endTextColor = Colors.black,
+  FontWeight titleBoldFontWeight = FontWeight.w700,
+  FontWeight? titleRegularFontWeight,
+  FontWeight? bodyTextFontWeight,
+  FontWeight? endTextFontWeight,
+  TextAlign? titleTextAlign,
+  TextAlign bodyTextAlign = TextAlign.justify,
+  TextAlign endTextAlign = TextAlign.end,
+  String? titleBold,
+  String? titleRegular,
+  String? bodyText,
+  String? endText,
+  required String buttonText,
+  required Function()? onPressed,
+}) => Center(
+  child: Container(
+    margin: EdgeInsets.symmetric(
+      horizontal: margin,
+    ),
+    padding: EdgeInsets.all(
+      padding,
+    ),
+    decoration: BoxDecoration(
+      color: isTransparent ? backgroundColor.withOpacity(
+        opacity,
+      ) : backgroundColor,
+      borderRadius: BorderRadius.circular(
+        borderRadius,
+      ),
+    ),
+    child: Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children:
+      [
+        RichText(
+          text: TextSpan(
+            children:
+            [
+              TextSpan(
+                text: titleBold,
+                style: TextStyle(
+                  fontSize: titleBoldTextSize,
+                  fontWeight: titleBoldFontWeight,
+                  color: titleBoldTextColor,
+                ),
+              ),
+              TextSpan(
+                text: titleRegular,
+                style: TextStyle(
+                  fontSize: titleRegularTextSize,
+                  fontWeight: titleRegularFontWeight,
+                  color: titleRegularTextColor,
+                ),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 10.0,
+        ),
+        Text(
+          bodyText!,
+          style: TextStyle(
+            fontSize: bodyTextSize,
+            fontWeight: bodyTextFontWeight,
+            color: bodyTextColor,
+          ),
+          textAlign: bodyTextAlign,
+        ),
+        SizedBox(
+          height: 14.0,
+        ),
+        SizedBox(
+          width: double.infinity,
+          child: Text(
+            endText!,
+            style: TextStyle(
+              fontSize: endTextSize,
+              fontWeight: endTextFontWeight,
+              color: endTextColor,
+            ),
+            textAlign: endTextAlign,
+          ),
+        ),
+        SizedBox(
+          height: 27.0,
+        ),
+        defaultButton(
+            onPressed: onPressed,
+            text: buttonText,
+        ),
+      ],
+    ),
+  ),
+);
