d75b4d472380e0bf076b7985033a274632c1928b
Sherif A Shahin <125300187+SherifAShahin@users.noreply.github.com>
2023-02-19 17:53:52 +0200

    Create call_example.dart

---
+++ call_example.dart
@@ -0,0 +1,13 @@
+//import message_container_template_file_path
+
+defaultNoteMessage(
+          titleBold: 'Welcome ',
+          titleRegular: 'to BookMark,',
+          bodyText: 'BookMark’s goal is to improve your reading experience and make it easier to explore more books you would like.',
+          endText: 'have a nice day ^_^',
+          buttonText: 'Get Started',        //required
+          onPressed: ()                     //required
+          {
+            
+          },
+        )
